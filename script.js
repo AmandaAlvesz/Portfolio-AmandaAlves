efc19ec2e91cf5790b99dab09530ed15b40b34de
Amanda Alves <crisamandaalves.24@gmail.com>
2025-05-01 15:17:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,4 +31,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     type();
+
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
 });
